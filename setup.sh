f4017d65c5df00c7de7e2d898d740d032791be28
HAIZAKURA <hhbilly99@gmail.com>
2025-06-01 11:50:05 +0900

    update: setup shell

--- setup.sh
+++ setup.sh
@@ -11,19 +11,11 @@ echo "Install Homebrew (will not install if already exists)"
 
 # create symlinks
 echo "Create symlinks of dotfiles (will not override if already exists)"
-[ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config $HOME/.config || ln -s $HOME/dotfiles/.config $HOME/.config
+[ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config/* $HOME/.config || ln -s $HOME/dotfiles/.config $HOME
 
 # fish shell
-echo "Install fish shell & add fish to /etc/shells (will not install if already exists)"
-[ -x "$(command -v fish)" ] || brew install fish | echo $(which fish) | sudo tee -a /etc/shells
-
-# change default shell to fish
-echo "Change shell to fish"
-chsh -s $(which fish)
-
-# run fish
-echo "Run fish shell"
-fish
+echo "Install fish shell & add fish to /etc/shells & change shell to fish & run fish"
+[ -x "$(command -v fish)" ] || brew install fish || echo $(which fish) | sudo tee -a /etc/shells || chsh -s $(which fish) || fish
 
 # kitty
 echo "Install kitty terminal (will not install if already exists)"
@@ -50,5 +42,5 @@ echo "Install mise (will not install if already exists)"
 [ -x "$(command -v fzf)" ] || brew install fzf
 
 # fonts
-echo "Install Maple Mono NF CN fonts (will not install if already exists)"
+echo "Install Maple Mono NF CN fonts"
 brew install --cask font-maple-mono-nf-cn
