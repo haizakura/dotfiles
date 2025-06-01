a1eced3dd7226241ecbf30dc3f69d4e5600c8316
HAIZAKURA <hhbilly99@gmail.com>
2025-06-01 11:00:12 +0900

    add: setup shell

---
+++ setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# package manager: Homebrew
+# cask: kitty
+# formulae: fish mise starship bat zoxide fzf
+# fonts: Maple Mono NF CN
+
+# Homebrew
+echo "Install Homebrew (will not install if already exists)"
+[ -x "$(command -v brew)" ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# create symlinks
+echo "Create symlinks of dotfiles (will not override if already exists)"
+[ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config $HOME/.config || ln -s $HOME/dotfiles/.config $HOME/.config
+
+# fish shell
+echo "Install fish shell (will not install if already exists)"
+[ -x "$(command -v fish)" ] || brew install fish
+
+# add fish to /etc/shells
+echo "Add fish to /etc/shells"
+echo $(which fish) | sudo tee -a /etc/shells
+
+# change default shell to fish
+echo "Change shell to fish"
+chsh -s $(which fish)
+
+# run fish
+echo "Run fish shell"
+fish
+
+# kitty
+echo "Install kitty terminal (will not install if already exists)"
+[ -x "$(command -v kitty)" ] || brew install --cask kitty
+
+# mise
+echo "Install mise (will not install if already exists)"
+[ -x "$(command -v mise)" ] || brew install mise
+
+ # starship
+  echo "Install starship (will not install if already exists)"
+[ -x "$(command -v starship)" ] || brew install starship
+
+# bat
+ echo "Install bat (will not install if already exists)"
+[ -x "$(command -v bat)" ] || brew install bat
+
+# zoxide
+ echo "Install zoxide (will not install if already exists)"
+[ -x "$(command -v zoxide)" ] || brew install zoxide
+
+# fzf
+ echo "Install fzf (will not install if already exists)"
+[ -x "$(command -v fzf)" ] || brew install fzf
+
+# fonts
+echo "Install Maple Mono NF CN fonts (will not install if already exists)"
+brew install --cask font-maple-mono-nf-cn
